045ac942f8a57206e9ec7830ad735c5f8bed8145
Thonipho <thonipho@icloud.com>
2021-09-21 15:12:37 +0200

    Code College

---
+++ Java Masterclass Remaster/JavaMasterclass/src/main/java/JavaTutorials/PositiveNegativeZero.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JavaTutorials;
+
+/**
+ *
+ * @author User
+ */
+public class PositiveNegativeZero {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        checkNumber(5);
+    }
+
+    public static void checkNumber(int number) {
+        if (number > 0) {
+            System.out.println("positive");
+        } else if (number < 0) {
+            System.out.println("negative");
+        } else {
+            System.out.println("zero");
+        }
+    }
+}
