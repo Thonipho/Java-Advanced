d533cc73933765687d0e45ef0243ca92b7ba679c
Thonipho <thonipho@icloud.com>
2021-10-17 07:28:43 +0200

    Code College

--- Java Masterclass Remaster/JavaMasterclass/src/main/java/ControlFlowStatements/ReadingUserInput.java
+++ Java Masterclass Remaster/JavaMasterclass/src/main/java/ControlFlowStatements/ReadingUserInput.java
@@ -17,20 +17,29 @@ public class ReadingUserInput {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         Scanner scanner = new Scanner(System.in);
-        
         System.out.println("Enter your year of birth: ");
-        int yearOfBirth = scanner.nextInt();
-        scanner.nextLine(); //handle next line character (enter key)
-        
-        System.out.println("Enter your name: ");
-        String name = scanner.nextLine();
-        int age = 2021 - yearOfBirth;
-        
-        System.out.println("Your name is " + name + ", and you are " + age + " years old");
-        
+        boolean hasNextInt = scanner.hasNextInt();
+
+        if (hasNextInt) {
+            
+            int yearOfBirth = scanner.nextInt();
+            scanner.nextLine(); //handle next line character (enter key)
+
+            System.out.println("Enter your name: ");
+            String name = scanner.nextLine();
+            int age = 2021 - yearOfBirth;
+
+            if (age >= 0 && age <= 100) {
+                System.out.println("Your name is " + name + ", and you are " + age + " years old");
+            } else {
+                System.out.println("Invalid year of birth");
+            }
+
+        }
+
         scanner.close();
     }
-    
+
 }
