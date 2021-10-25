e71ac9ffbf765df840b95cc8ce2842696ad234d4
Thonipho <thonipho@icloud.com>
2021-10-25 10:50:07 +0200

    Code College

---
+++ Java Masterclass Remaster/JavaMasterclass/src/main/java/Inheritance/Dog.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Inheritance;
+
+/**
+ *
+ * @author User
+ */
+public class Dog extends Animal {
+    
+    private int eyes;
+    private int legs;
+    private int tail;
+    private int teeth;
+    private String coat;
+
+    public Dog(String name, int size, int weight, int eyes, int legs, int tail, int teeth, String coat) {
+        super(name, 1, 1, size, weight);
+        this.eyes = eyes;
+        this.legs = legs;
+        this.tail = tail;
+        this.teeth = teeth;
+        this.coat = coat;
+    }
+    
+    private void chew(){
+        System.out.println("Animal chewed its food");
+    }
+
+    @Override
+    public void eat() {
+        chew();
+        super.eat(); 
+    }
+}
