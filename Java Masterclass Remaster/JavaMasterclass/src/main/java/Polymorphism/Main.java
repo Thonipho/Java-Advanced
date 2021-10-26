e47c5d525e1b295edc0ebc3e03b00fe119f01ee9
Thonipho <thonipho@icloud.com>
2021-10-26 15:05:33 +0200

    Code College

--- Java Masterclass Remaster/JavaMasterclass/src/main/java/Polymorphism/Main.java
+++ Java Masterclass Remaster/JavaMasterclass/src/main/java/Polymorphism/Main.java
@@ -14,6 +14,11 @@ class Movie{
     public String plot(){
         return "No plot here";
     }
+
+    public String getName() {
+        return name;
+    }
+    
 }
 
 class Jaws extends Movie {
@@ -72,6 +77,29 @@ public class Main {
 
     public static void main(String[] args) {
         
+        for(int i =0; i<11; i++) {
+            Movie movie = randomMovie();
+            System.out.println("Movie #" + i + " : " + movie.getName() + "\n" + 
+                    "Plot: " + movie.plot() + "\n");
+        }
     }
     
+    public static Movie randomMovie(){
+        int randomNumber = (int) (Math.random()*5)+1;
+        System.out.println("Random number generated was: " + randomNumber);
+        
+        switch (randomNumber){
+            case 1:
+                return new Jaws();
+            case 2:
+                return new IndependenceDay();
+            case 3:
+                return new MazeRunner();
+            case 4:
+                return new StarWars();
+            case 5:
+                return new Forgetable();
+        }
+        return null;
+    }
 }
