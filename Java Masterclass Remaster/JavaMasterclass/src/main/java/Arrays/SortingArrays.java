8a1e089d7ed57945977338533636ee1c2077c0c8
Thonipho <thonipho@icloud.com>
2021-11-08 10:26:48 +0200

    Code College

--- Java Masterclass Remaster/JavaMasterclass/src/main/java/Arrays/SortingArrays.java
+++ Java Masterclass Remaster/JavaMasterclass/src/main/java/Arrays/SortingArrays.java
@@ -32,10 +32,37 @@ public class SortingArrays {
         }
     }
     
+    public static int[] sortArray( int [] array) {
+        int[] sortedArray = new int[array.length];
+        
+        for(int i=0; i<array.length; i++){
+            sortedArray[i] = array[i];
+        }
+        
+        boolean flag = true;
+        int temp;
+        while(flag) {
+            flag = false;
+            for(int i=0; i<sortedArray.length-1; i++) {
+                if(sortedArray[i] < sortedArray[i+1]) {
+                    temp = sortedArray[i];
+                    sortedArray[i] = sortedArray[i+1];
+                    sortedArray[i+1] = temp;
+                    flag = true;
+                }
+            }
+        }
+        
+        return sortedArray;
+    }
+    
     public static void main(String[] args) {
         
-        int[] myInteger = getInteger(5);
-        
+        int[] myIntegers = getInteger(5);
+        int[] sorted = sortArray(myIntegers);
+        printArray(myIntegers);
+        System.out.println(".......................");
+        printArray(sorted);
     }
     
 }
