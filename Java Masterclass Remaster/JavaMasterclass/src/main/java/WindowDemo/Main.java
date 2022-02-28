bf68e0a0466bda9cbf5f50ea2bbe36e5aa3b8506
Thonipho <thonipho@icloud.com>
2022-02-28 11:57:34 +0200

    Code College

---
+++ Java Masterclass Remaster/JavaMasterclass/src/main/java/WindowDemo/Main.java
@@ -0,0 +1,10 @@
+package WindowDemo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MyWindow myWindow = new MyWindow("Complete Java");
+        myWindow.setVisible(true);
+    }
+    
+}
